9c99add319be1d12c9dfca0894f3f62a4856febd
GuilhermeCampano <guilherme_campano@yahoo.com.br>
2015-06-07 09:18:01 -0300

    update http

--- js/app.js
+++ js/app.js
@@ -18,6 +18,7 @@ app.config(['$routeProvider',
         controller: 'ShotsController',
 		resolve: {
             dribbbleApi : function ($http, dribbble ) {
+				console.log(dribbble);
 				return dribbble;
             }
 		}
