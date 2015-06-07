13757758ef1e7130c6a3a6fc103cbb9a0912ce13
GuilhermeCampano <guilherme_campano@yahoo.com.br>
2015-06-07 18:19:55 -0300

    fechar menu mobile ao clicar

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -4,7 +4,7 @@ app.controller('MainController', function($scope, $http) {
   $scope.mobileMenu = {
 	
 	mobileMenu : document.getElementById("mobile-demo"),
-	active : function(){
+	open : function(){
 		this.mobileMenu.setAttribute("class", "side-nav active");
 	},
 	close : function(){
