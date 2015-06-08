16219cb54c26582b24d72acf98b47cc0df6961c5
GuilhermeCampano <guilherme_campano@yahoo.com.br>
2015-06-08 08:43:02 -0300

    Update ShotController.js

--- js/controllers/ShotController.js
+++ js/controllers/ShotController.js
@@ -1,14 +1,17 @@
 app.controller('ShotController', function($scope, dribbbleApi, $routeParams, $sce) { 
 	
-	//===========PRIVATE=========//
-	var id = $routeParams.id;
+   //===========PRIVATE=========//
+   
+   //id do shot
+   var id = $routeParams.id;
 	
-	//===========PUBLIC==========//
-	//único shot
+   //===========PUBLIC==========//
+   
+   //popula $scope.shot como os seus rescpectivos dados
     dribbbleApi.shot(id).then(function (data){
         $scope.shot = data.data;
-		$scope.shot.description =  $sce.trustAsHtml($scope.shot.description);
-		$scope.shot.created_at=new Date($scope.shot.created_at);
+		$scope.shot.description =  $sce.trustAsHtml($scope.shot.description); //permite renderizar tags html
+		$scope.shot.created_at=new Date($scope.shot.created_at); //converte para data 
     });
 	
 });
